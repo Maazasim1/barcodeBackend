14b0593ab7a4f4c7baf64e59b6267c9516bb9ce2
maaz <mazzasimqw@gmail.com>
2024-06-07 15:03:05 +0500

    added response for error

--- src/index.js
+++ src/index.js
@@ -11,28 +11,33 @@ const port = process.env.PORT;
 const app = express();
 
 mongoose
-.connect(process.env.CONNECTION_STRING)
-.then(() => {
-  console.log(`Connected to MongoDB Database`);
-})
-.catch((error) => {
-  console.error('Error connecting to MongoDB:', error);
-});
+  .connect(process.env.CONNECTION_STRING)
+  .then(() => {
+    console.log(`Connected to MongoDB Database`);
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
 
 
-app.get('/',(req,res)=>{
-    res.send("spinning up serverless functions")
+app.get('/', (req, res) => {
+  res.send("spinning up serverless functions")
 })
 
 app.get('/uploadToDatabase', async (req, res) => {
-    await uploadToMongo();
+  await uploadToMongo();
   res.send('all files have been uploaded to the database');
 });
 
-app.get('/searchGtin/:gtin',async (req,res)=>{
-    const gtin=req.params.gtin;
-    const result= await searchGtin(gtin);
-    res.send(result)
+app.get('/searchGtin/:gtin', async (req, res) => {
+  const gtin = req.params.gtin;
+  const result = await searchGtin(gtin);
+  if (result === "gtin not found") {
+    return res.status(200).json({ "error": "gtin not found" })
+  }
+  else {
+    return res.send(result)
+  }
 })
 
 app.listen(port, () => {
